c1f2bc72d326af316a2e8fa15d4d8a610b9d76ce
3v4n-Skinner <evan.skinner@Digipen.edu>
2025-11-21 09:27:32 -0800

    Inital

---
+++ Assets/Scripts/RandAsteroidScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class RandAsteroidScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody2D asteroid_rb2d;
+    public GameObject asteroid;
+
+    //Preset ranges
+    static private float maxSize = 2;
+    [SerializeField] float maxSpeed = 10;
+    static private float maxDirection = 360;
+    void Start()
+    {
+        float sizeOfAsteroid = Random.Range(0.5f, maxSize);
+        asteroid.transform.localScale = new Vector3(sizeOfAsteroid,sizeOfAsteroid,1);
+        asteroid.transform.Rotate(0,0,Random.Range(0,maxDirection +1));
+        asteroid_rb2d.AddForce(Random.insideUnitCircle * Random.Range(1,maxSpeed));
+        //lol
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
