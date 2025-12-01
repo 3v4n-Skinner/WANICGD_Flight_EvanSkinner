035390b1f763daa3bdb9b6306ba194def969e557
3v4n-Skinner <evan.skinner@Digipen.edu>
2025-12-01 09:23:19 -0800

    Finished
    Finished flight project no issues.

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -1,30 +1,54 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+
 
 public class Player_Controller : MonoBehaviour
 {
-    public float thrustForce;
+    private float elapsedTime = 0f;
+    private float score = 0f;
+    public float scoreMultiplyer;
+    public float thrustForce = 1f;
     public Rigidbody2D player_rb2d;
+    public UIDocument uiDoc;
+    private Button restartButton;
+    private Label scoreLabel;
+    public GameObject explosionEffect;
     
     void Start()
     {
-        
+        scoreLabel = uiDoc.rootVisualElement.Q<Label>("ScoreLabel");
+        restartButton = uiDoc.rootVisualElement.Q<Button>("RestartButton");
+        restartButton.clicked += ReloadScene;
+        restartButton.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        elapsedTime += Time.deltaTime;
+        score = Mathf.FloorToInt(elapsedTime * scoreMultiplyer);
+
+        scoreLabel.text = ("Score: " + score);
+
         if (Mouse.current.leftButton.isPressed)
         {
             //Calculate mouse direction
             Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
             Vector2 Direction = (mouse_pos - transform.position).normalized;
             transform.up = Direction;
-            player_rb2d.AddForce(Direction * thrustForce);
+            player_rb2d.AddForce(Direction * thrustForce * Time.deltaTime,ForceMode2D.Impulse);
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
+        Instantiate(explosionEffect,transform.position, transform.rotation);
+        restartButton.visible = true;
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
