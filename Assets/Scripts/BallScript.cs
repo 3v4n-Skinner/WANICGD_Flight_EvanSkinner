614b39b3dbcce19c76fc0d284f217ecc7f0b00ad
3v4n-Skinner <evan.skinner@Digipen.edu>
2025-12-04 08:18:15 -0800

    Adding comments and categorizing
    worked on adding comments and categorizing variables in the code.

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -3,33 +3,46 @@ using UnityEngine.UIElements;
 
 public class BallScript : MonoBehaviour
 {
+    //Players rigidbody
     private Rigidbody2D ball_rb2d;
+
+    [Header("Velocity related below")]
     public float range_of_power = 25f;
     public float scale_Power = 20f;
     private float powerX = 0f;
     private float powerY = 0f;
+
+    [Header("SFX related below")]
     public AudioClip hit_Sound;
+
+    [Header("UI related below")]
     public UIDocument ballUI;
     private Label bounces;
     private static int score = 0;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+
     void Start()
     {
+        //Gets the balls rigidbody in an ineffective way and sets the balls velocity to a random speed
         ball_rb2d = transform.GetComponent<Rigidbody2D>();
         powerX = Random.Range(1,range_of_power);
         powerY = Random.Range(1, range_of_power);
         ball_rb2d.AddForceX(powerX * scale_Power);
         ball_rb2d.AddForceY(powerY * scale_Power);
+
+        //Gets the label from the UI Doc
         bounces = ballUI.rootVisualElement.Q<Label>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Updates the score UI
         bounces.text = "Ball Bounces: " + score;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //Plays a sound effect when balls collide with things, and Only adds score when the balls collide
         AudioSource.PlayClipAtPoint(hit_Sound,new Vector3());
         if (collision.transform.name.Contains("Ball"))
         {
--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -6,17 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class Player_Controller : MonoBehaviour
 {
-    private float elapsedTime = 0f;
-    private float score = 0f;
-    public float scoreMultiplyer;
+
+    //Player related Variables
     public float thrustForce = 1f;
     public Rigidbody2D player_rb2d;
+    public GameObject explosionEffect;
+
+    //UI Used/related Variables
+    [Header("UI related")]
     public UIDocument uiDoc;
     private Button restartButton;
     private Label scoreLabel;
-    public GameObject explosionEffect;
+    private float elapsedTime = 0f;
+    private float score = 0f;
+    public float scoreMultiplyer;
+
+    
+    [Header("SFX related")]
     public AudioClip asteroid_Collision;
     public AudioSource rocketMoving;
+    private bool rocketActive;
     void Start()
     {
         scoreLabel = uiDoc.rootVisualElement.Q<Label>("ScoreLabel");
@@ -27,40 +36,58 @@ public class Player_Controller : MonoBehaviour
 
     void Update()
     {
+        //Increments the score and changes the score label
         elapsedTime += Time.deltaTime;
         score = Mathf.FloorToInt(elapsedTime * scoreMultiplyer);
-
         scoreLabel.text = ("Score: " + score);
 
         if (Mouse.current.leftButton.isPressed)
         {
-            //Calculate mouse direction
+            //Calculate mouse direction and moves the rocket
             Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
             Vector2 Direction = (mouse_pos - transform.position).normalized;
             transform.up = Direction;
-            player_rb2d.AddForce(Direction * thrustForce * Time.deltaTime,ForceMode2D.Impulse);
-            
+            player_rb2d.AddForce(Direction * thrustForce * Time.deltaTime, ForceMode2D.Impulse);
         }
-        if(Input.GetMouseButtonDown(0))
-        {
-            rocketMoving.Play();
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            rocketMoving.Stop();
-        }
-        
+
+        //Plays the rocket thrusting sound when the player clicks down
+        RocketSFX();
+
+
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //This is everthing that has to do with the player dying
         Destroy(gameObject);
         AudioSource.PlayClipAtPoint(asteroid_Collision,new Vector3());
         Instantiate(explosionEffect,transform.position, transform.rotation);
         restartButton.visible = true;
     }
 
+    //Reloads the current scene(aka the ship game), this method is ment to be used in buttons
     void ReloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    //Method for nowing how to handel moving SFX
+    void RocketSFX()
+    {
+        //Checks when the player presses down and releases
+        if (Mouse.current.leftButton.wasPressedThisFrame){
+            rocketActive = true;
+        } else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            rocketActive = false;
+        }
+
+        //Plays the rocket thrusting sound when rocketActive is true and stops when its false
+        if (rocketActive && rocketMoving.isPlaying == false)
+        {
+            rocketMoving.Play();
+        } else if (!rocketActive)
+        {
+            rocketMoving.Stop();
+        }
     }
 }
